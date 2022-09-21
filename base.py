8a68f3db2ea0a4d920190d84a8e37c73e07bf7e0
John Huntley <johnh@Johns-MacBook-Pro.local>
2022-09-21 08:00:32 -0400

    Latest

--- base.py
+++ base.py
@@ -14,11 +14,21 @@ highcon_img = cv.imread("highcontrast.jpeg")
 
 n = gray_img.reshape(-1, 1)
 
-data = pd.DataFrame(n)
+base_data = pd.DataFrame(n)
+counts_data = base_data.value_counts()
+data = pd.DataFrame(counts_data)
 
-data.value_counts().hist(bins=30)
-p = data.value_counts().hist(bins=30)
+data.sort_index(inplace=True)
+data.rename(columns={0:"counts"}, inplace=True)
+
+data['Probability'] = data['counts'] / data['counts'].sum()
+
+
+
+
+#data.value_counts().hist(bins=30)
+#p = data.value_counts().hist(bins=30)
 
 if __name__== "__main__":
-    p.plt()
+    print(data)
 
