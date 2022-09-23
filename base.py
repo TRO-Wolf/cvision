1c6cfc3c408ae054674f0f998c341a957202a36b
John Huntley <johnh@Johns-MacBook-Pro.local>
2022-09-22 20:53:13 -0400

    Latest

--- base.py
+++ base.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from unicodedata import name
 import numpy as np
 import pandas as pd
@@ -11,7 +12,28 @@ color_img = cv.imread("colordog.jpg")
 lowcon_img = cv.imread("lowcontrast.jpg")
 highcon_img = cv.imread("highcontrast.jpeg")
 
+im_list = [gray_img, color_img, lowcon_img, highcon_img]
 
+
+def shape_it(image):
+    return image.reshape(-1, 1)
+
+def organize_data(image):
+    n = shape_it(image)
+    base_data = pd.DataFrame(n)
+    counts_data = base_data.value_counts()
+    data = pd.DataFrame(counts_data)
+    data.sort_index(inplace=True)
+    data.rename(columns={0:"counts"}, inplace=True)
+    data['Probability'] = data['counts'] / data['counts'].sum()
+    data['Intensity'] = reduce(lambda x, y: x + y, data.index)
+
+    return data
+
+
+
+
+'''
 n = gray_img.reshape(-1, 1)
 
 base_data = pd.DataFrame(n)
@@ -20,15 +42,25 @@ data = pd.DataFrame(counts_data)
 
 data.sort_index(inplace=True)
 data.rename(columns={0:"counts"}, inplace=True)
-
 data['Probability'] = data['counts'] / data['counts'].sum()
+'''
 
 
+
+
+plt.imshow(gray_img)
+
+im_data = organize_data(gray_img)
 
 
 #data.value_counts().hist(bins=30)
 #p = data.value_counts().hist(bins=30)
 
 if __name__== "__main__":
-    print(data)
+    #print(data)
+    print(im_data)
+    print("The mean of the the image is {}".format(im_data['Probability'].mean()))
+    print("The variance of the image is {}".format(im_data['Probability'].var()))
+
+    plt.show()
 
